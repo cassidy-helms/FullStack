590b2db887e175e9d2e354b7873e075ee06f35a8
Helms, Cassidy <cassidy.l.helms@metlife.com>
2017-05-02 16:56:05 -0400

    adds projects rmCore and rmWeb

---
+++ rmCore/src/main/java/com/chelms/fullstack/erkCore/App.java
@@ -0,0 +1,13 @@
+package com.chelms.fullstack.erkCore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
